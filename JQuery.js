b8d488c35938475da8b2084ff4be78d39ae8c575
Steven <heatrocks63134@gmail.com>
2017-08-10 01:35:50 -0400

    finished all responsiveness and jquery

--- JQuery.js
+++ JQuery.js
@@ -12,6 +12,17 @@ $(document).ready(function() {
     }
   );
 
+  $('#skills').hover(
+    function() {
+      $(this).addClass('lijquery');
+    },
+
+    function() {
+      $(this).removeClass('lijquery');
+    }
+  );
+
+
   $('#logo').hover(
     function() {
       $('#logo').addClass('logojquery');
@@ -22,15 +33,15 @@ $(document).ready(function() {
     }
   );
 
-  // $('.parallax-inner0').hover(
-  //   function() {
-  //     $(this).addClass('parallaxjquery');
-  //   },
-  //
-  //   function() {
-  //     $(this).removeClass('parallaxjquery');
-  //   }
-  // );
+  $('.parallax-inner0').hover(
+    function() {
+      $(this).addClass('parallaxjquery');
+    },
+
+    function() {
+      $(this).removeClass('parallaxjquery');
+    }
+  );
 
   $('.parallax-inner1').hover(
     function() {
@@ -71,4 +82,38 @@ $(document).ready(function() {
       $(this).removeClass('parallaxjquery');
     }
   );
+
+  $('.parallax-inner5').hover(
+    function() {
+      $(this).addClass('parallaxjquery');
+    },
+
+    function() {
+      $(this).removeClass('parallaxjquery');
+    }
+  );
+
+  $('#welcome').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $('.parallax0').offset().top
+    }, 800);
+  });
+
+  $('#skills').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $('.parallax1').offset().top
+    }, 800);
+  });
+
+  $('#work').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $('.parallax4').offset().top
+    }, 800);
+  });
+
+  $('#contact').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $('.parallax5').offset().top
+    }, 800);
+  });
 });
